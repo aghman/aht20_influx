be8487440acdb1260054273ab931d9ff5be2a1e1
Austin Hill <aghman@gmail.com>
2021-01-23 22:33:21 -0800

    Adding F to temp output

--- aht20_influx.py
+++ aht20_influx.py
@@ -8,6 +8,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while True:
-    print("\nTemperature: %0.1f C" % sensor.temperature)
+    temp_farenheight = sensor.temperature * (9/5) + 32
+    print("\nTemperature: %0.1f C, %0.1f" % sensor.temperature % temp_farenheight)
     print("Humidity: %0.1f %%" % sensor.relative_humidity)
     time.sleep(2)
